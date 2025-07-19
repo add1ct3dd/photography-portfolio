0ca275d52a1ba79c3790ecaf8d72eea817854f0c
Matthew Evans <add1ct3dd@gmail.com>
2025-07-19 19:47:38 +0100

    Fix gulpfile.js, add some more images.

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -65,7 +65,7 @@ gulp.task('minify-js', function () {
 gulp.task('build', gulp.series('sass', 'minify-js'));
 
 // resize images
-gulp.task('resize', gulp.series('delete', 'resize-images'));
+gulp.task('resize', gulp.series('resize-images', 'delete'));
 
 // default task
 gulp.task('default', gulp.series('build', 'resize'));
